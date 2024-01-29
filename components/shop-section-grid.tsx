38b2823cd40a9b9fe30657fc8f5cfca363064941
dnzzzl <Denzel2603@gmail.com>
2024-01-29 15:04:30 -0400

    fixed shop-grid and products css layout

--- components/shop-section-grid.tsx
+++ components/shop-section-grid.tsx
@@ -6,8 +6,8 @@ export const ShopGrid: FC<ShopProps> = ({ allProducts, preview }: ShopProps) => 
 
 
   return (
-    <section className="container mx-auto pb-12 px-4 md:px-12">
-      <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-4">
+    <section className=" mx-auto pb-12 px-4 md:px-12">
+      <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {allProducts.edges.map((product) => (
           <ProductCard {...product} />
         ))}
