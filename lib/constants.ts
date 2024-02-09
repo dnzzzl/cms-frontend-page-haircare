4a3f1d08883853ba48d8b6afe5f956515fad5b91
dnzzzl <Denzel2603@gmail.com>
2024-02-09 18:35:01 -0400

    icons and share metadata

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const EXAMPLE_PATH = 'cms-wordpress'
 export const CMS_NAME = 'WordPress'
 export const CMS_URL = '#'
 export const HOME_OG_IMAGE_URL =
-  'http://wordpress.elizabeth-haircare.com/wp-content/uploads/2024/01/IMG_MJL_☣️-Sharp-Natural-🍀_20210315_1655272332-scaled.jpg'
+  'https://wordpress.elizabeth-haircare.com/wp-content/uploads/2024/01/IMG_MJL_☣️-Sharp-Natural-🍀_20210315_1655272332-scaled.jpg'
 export const HERO_IMG_URL = "https://wordpress.elizabeth-haircare.com/wp-content/uploads/2024/01/hero-02.png"
 export const HERO_SHOP_IMG_URL = "https://wordpress.elizabeth-haircare.com/wp-content/uploads/2024/01/hero-01.png"
 export const LOGO_SEETHRU_IMG_URL = "https://wordpress.elizabeth-haircare.com/wp-content/uploads/2024/01/enhanced-image-3-1-e1706740050669.png"
