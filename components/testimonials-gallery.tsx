d458301941e27d2ec5fdd03d881b7ea7880d95b2
dnzzzl <Denzel2603@gmail.com>
2024-03-25 13:57:47 -0400

    Refactored testimonials gallery with carousel component

--- components/testimonials-gallery.tsx
+++ components/testimonials-gallery.tsx
@@ -1,43 +1,61 @@
-import React from 'react'
-import {Parallax} from './parallax'
-import { AvatarCn,AvatarFallback,AvatarImage } from './avatar-shadcn'
-import Link from 'next/link'
+import React from "react";
+import { Parallax } from "./parallax";
+import { AvatarCn, AvatarFallback, AvatarImage } from "./avatar-shadcn";
+import Link from "next/link";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./carousel";
 
 export type TestimonialType = {
-    name: string,
-    content: string,
-    imageSrc: string,
-    handle: string,
-    link: string,
-    initials: string,
-}
+  name: string;
+  content: string;
+  imageSrc: string;
+  handle: string;
+  link: string;
+  initials: string;
+};
 
-const TestimonialsGallery = (props: { listoftestimonials: TestimonialType[] }) => {
+const TestimonialsGallery = (props: {
+  listoftestimonials: TestimonialType[];
+}) => {
   return (
-    
-    <div className='h-[90vh] sticky top-8 bg-background w-full overflow-hidden '>
-        <h1 className='text-4xl md:text-7xl mb-10 md:mb-4 justify-center font-bold tracking-tighter leading-tight'>Confia en nuestros clientes y sus opiniones</h1>
-        <div className='flex flex-row md:flex-wrap overflow-hidden gap-5 pt-20'>
-            {props.listoftestimonials.map((testimonial, index) => (
-            <Parallax key={index} speed={-.2-index-Math.random()*2} className="mx-auto">
-                <div className=" w-[300px] bg-gradient-to-tr from-pink-200 via-red-300 to-background rounded-lg border-red-300 border-2 p-4">
-                <h1 className='font-semibold text-xl mb-2'>{testimonial.name}</h1>
+    <div className="sticky top-8 h-[90vh] w-full overflow-hidden bg-background ">
+      <h1 className="mb-10 justify-center text-4xl font-bold leading-tight tracking-tighter md:mb-4 md:text-7xl">
+        Confia en nuestros clientes y sus opiniones
+      </h1>
+      <Carousel
+        className="mx-10"
+      >
+        <CarouselContent className="">
+          {props.listoftestimonials.map((testimonial, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 my-auto">
+              <div className=" mx-auto rounded-lg border-2 border-red-300 bg-gradient-to-tr from-pink-200 via-transparent to-red-200 p-4">
+                <h1 className="mb-2 text-xl font-semibold">
+                  {testimonial.name}
+                </h1>
                 <p>"{testimonial.content}"</p>
-                <div className='flex items-center gap-2 m-2'>
-                    - <AvatarCn>
+                <div className="m-2 flex items-center gap-2">
+                  -{" "}
+                  <AvatarCn>
                     <AvatarImage src={testimonial.imageSrc} />
                     <AvatarFallback>{testimonial.initials}</AvatarFallback>
-                    </AvatarCn>
-                    <Link href={testimonial.link}>{testimonial.handle}</Link>
+                  </AvatarCn>
+                  <Link href={testimonial.link}>{testimonial.handle}</Link>
                 </div>
-                </div>
-            </Parallax>
-            ))}
-        </div>
-        
-        
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+      <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default TestimonialsGallery
+export default TestimonialsGallery;
